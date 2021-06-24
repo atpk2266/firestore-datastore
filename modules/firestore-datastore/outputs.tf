600a67bdd12d70fb7bd9b14730d4e869ea963049
atpk2266 <82212964+atpk2266@users.noreply.github.com>
2021-06-24 15:01:57 +0530

    Create outputs.tf

---
+++ modules/firestore-datastore/outputs.tf
@@ -0,0 +1,10 @@
+output "usage_IAM_roles" {
+  description = "Basic IAM role(s) that are generally necessary for using the resources in this module. See https://cloud.google.com/iam/docs/understanding-roles."
+  value = [
+    "roles/datastore.user",
+  ]
+}
+
+output "firestore-db" {
+  value = "db"
+}
